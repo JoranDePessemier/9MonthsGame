3b5330a4674d50735fd8894c17bbdd43088a3350
JoranDePessemier <joran.de.pessemier@student.howest.be>
2023-02-22 23:28:27 +0100

    ADD Basic statemachine

---
+++ Assets/Scripts/StateMachine/base/State.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State 
+{
+    public StateMachine StateMachine { get; set; }
+
+    public virtual void OnExit() { }
+
+
+    public virtual void OnEnter() { }
+
+    public virtual void OnSuspend() { }
+
+
+    public virtual void OnResume() { }
+}
